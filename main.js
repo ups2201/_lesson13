1c293ea0823e983b1fd6d60aba01dcfd0cc1b325
ups2201 <74589416+ups2201@users.noreply.github.com>
2024-07-04 07:52:24 +0000

    Deploying to gh-pages from @ ups2201/lesson13@159626ff74a283d8d80ae19c8bf3531ea567d1a0 🚀

--- main.js
+++ main.js
@@ -60,6 +60,56 @@ eval("\n\nmodule.exports = function (i) {\n  return i[1];\n};\n\n//# sourceURL=w
 
 /***/ }),
 
+/***/ "./src/images/1.svg":
+/*!**************************!*\
+  !*** ./src/images/1.svg ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/1.svg\");\n\n//# sourceURL=webpack://lesson13/./src/images/1.svg?");
+
+/***/ }),
+
+/***/ "./src/images/2.svg":
+/*!**************************!*\
+  !*** ./src/images/2.svg ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/2.svg\");\n\n//# sourceURL=webpack://lesson13/./src/images/2.svg?");
+
+/***/ }),
+
+/***/ "./src/images/3.svg":
+/*!**************************!*\
+  !*** ./src/images/3.svg ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/3.svg\");\n\n//# sourceURL=webpack://lesson13/./src/images/3.svg?");
+
+/***/ }),
+
+/***/ "./src/images/left_arrow.svg":
+/*!***********************************!*\
+  !*** ./src/images/left_arrow.svg ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/left_arrow.svg\");\n\n//# sourceURL=webpack://lesson13/./src/images/left_arrow.svg?");
+
+/***/ }),
+
+/***/ "./src/images/right_arrow.svg":
+/*!************************************!*\
+  !*** ./src/images/right_arrow.svg ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/right_arrow.svg\");\n\n//# sourceURL=webpack://lesson13/./src/images/right_arrow.svg?");
+
+/***/ }),
+
 /***/ "./src/styles.css":
 /*!************************!*\
   !*** ./src/styles.css ***!
@@ -146,7 +196,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ \"./src/styles.css\");\n/* harmony import */ var _carousel_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./carousel.js */ \"./src/carousel.js\");\n\n\n\nconst carouselElement = document.querySelector(\"ul\");\nnew _carousel_js__WEBPACK_IMPORTED_MODULE_1__.Carousel(carouselElement);\n\n\n//# sourceURL=webpack://lesson13/./src/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ \"./src/styles.css\");\n/* harmony import */ var _carousel_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./carousel.js */ \"./src/carousel.js\");\n/* harmony import */ var _images_1_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./images/1.svg */ \"./src/images/1.svg\");\n/* harmony import */ var _images_2_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./images/2.svg */ \"./src/images/2.svg\");\n/* harmony import */ var _images_3_svg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./images/3.svg */ \"./src/images/3.svg\");\n/* harmony import */ var _images_left_arrow_svg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./images/left_arrow.svg */ \"./src/images/left_arrow.svg\");\n/* harmony import */ var _images_right_arrow_svg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./images/right_arrow.svg */ \"./src/images/right_arrow.svg\");\n\n\n\n\n\n\n\n\nconst carouselElement = document.querySelector(\"ul\");\nnew _carousel_js__WEBPACK_IMPORTED_MODULE_1__.Carousel(carouselElement);\n\n\n//# sourceURL=webpack://lesson13/./src/main.js?");
 
 /***/ })
 
@@ -201,6 +251,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -215,6 +277,29 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && (!scriptUrl || !/^http(s?):/.test(scriptUrl))) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
